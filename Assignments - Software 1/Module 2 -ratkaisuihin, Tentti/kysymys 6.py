9e3cb72b93b5c9b1d60249a537edd9f736628e14
khevavasam <126215534+khevavasam@users.noreply.github.com>
2025-08-22 11:20:12 +0300

    completed tasks 4 and 5, added course exercises

---
+++ Assignments - Software 1/Module 2 -ratkaisuihin, Tentti/kysymys 6.py
@@ -0,0 +1,8 @@
+# Faulty program, infinite loop
+
+number = 1
+while number<5:
+    print(number)
+
+# This part is never reached:
+print("All ready.")
---
+++ Assignments - Software 1/course material/04.py
@@ -0,0 +1,94 @@
+# # rounds = int(input("How many greetings: "))
+# # finished_rounds = 0
+# # while finished_rounds<rounds:
+# #     print("Good morning")
+# #     finished_rounds = finished_rounds + 1
+# #
+# #
+# # command = input("Enter command: ")
+# # while command!="stop":
+# #     print("Executing command: " + command)
+# #     command = input("Enter command: ")
+# # print("Execution stopped.")
+#
+#
+# import random
+# dice1 = dice2 = rolls = 0
+# while (dice1!=6 or dice2!=6):
+#     dice1 = random.randint(1,6)
+#     dice2 = random.randint(1,6)
+#     rolls = rolls + 1
+# print(f"Rolled {rolls:d} times.")
+#
+#
+
+
+# first = 2
+# while first <= 9:
+#     second = 2
+#     while second <= 9:
+#         print(f"{first} times {second} is {first*second:d}")
+#         second = second + 1
+#     first = first + 1
+
+
+
+
+# import random
+# rounds = 0
+# total_rolls = 0
+#
+# while rounds < 100000:
+#     dice1 = dice2 = rolls = 0
+#     while (dice1!=6 or dice2!=6):
+#         dice1 = random.randint(1,6)
+#         dice2 = random.randint(1,6)
+#         rolls = rolls + 1
+#         # print(f"Rolled {rolls:d} times.")
+#         rounds = rounds + 1
+#         total_rolls = total_rolls + rolls
+#
+# average_rolls = total_rolls/rounds
+# print(f"Average rolls required: {average_rolls:6.2f}")
+
+
+
+#
+# command = input("Enter command: ")
+# while command!="stop":
+#     if command=="MAYDAY":
+#         break
+#     print("Executing command: " + command)
+#     command = input("Enter command: ")
+# else:
+#     print("Goodbye.")
+# print("Execution stopped.")
+
+
+# Faulty program, infinite loop
+
+# number = 1
+# while number<5:
+#     print(number)
+#     number = number + 1
+# print("All ready.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
