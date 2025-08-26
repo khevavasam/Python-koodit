3b6727fe4b72c80a2459291ac29bf75aae2c438e
khevavasam <126215534+khevavasam@users.noreply.github.com>
2025-08-26 10:27:15 +0300

    course material 05 added

---
+++ Assignments - Software 1/course material/05.py
@@ -0,0 +1,34 @@
+# names = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
+#
+# print(names[3])
+# print(names[1])
+# print(names[-2])
+# print(names[1:3])
+# print(names[2:])
+# print(names)
+# print(len(names))
+
+
+# names = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
+# # Illegal reference
+# print(names[4])
+
+
+
+
+# names = []
+#
+# name = input("Enter the first name or quit by pressing Enter: ")
+# while name!="":
+#     names.append(name)
+#     name = input("Enter the next name or quit by pressing Enter: ")
+#
+# for n in names:
+#     print(f"Hello, {n}!")
+
+
+
+
+# for number in range(0,36,5):
+#     print(number)
+
